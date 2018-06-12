ee41e431d5e3f501c09213308950e65d7803a63d
kang <316852753@qq.com>
2018-06-12 18:04:27 +0800

    增加统计权限常量

---
+++ sb-statistic/src/main/java/com/jfeat/am/module/statistics/constant/StatisticPermission.java
@@ -0,0 +1,10 @@
+package com.jfeat.am.module.statistics.constant;
+
+/**
+ * Created by kang on 2018/6/12.
+ */
+public class StatisticPermission {
+
+    public static final String STATISTIC_VIEW = "statistic.view";
+
+}

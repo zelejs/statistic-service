1cf6cf0f26747eb6878b0d14c7163c4ed2876549
vincent@pc <vincent@jfeat.com>
2018-08-04 15:17:56 +0800

    add Month timeline

--- sb-statement/src/main/java/com/jfeat/am/module/statement/services/statistics/Timeline.java
+++ sb-statement/src/main/java/com/jfeat/am/module/statement/services/statistics/Timeline.java
@@ -22,10 +22,24 @@ public class Timeline {
         LW1,     /// Latest week,
         LM1,     /// Latest month
         LM3,     /// Latest 3 months
-        Q1,      //今天第一季度
+        M1,      /// query by Month
+        M2,
+        M3,
+        M4,
+        M5,
+        M6,
+        M7,
+        M8,
+        M9,
+        M10,
+        M11,
+        M12,
+        Q1,      // 今年第一季度
         Q2,
         Q3,
         Q4,
+        Q12,     //上半年
+        Q34,     //下半年
     }
 
     /**
@@ -85,7 +99,14 @@ public class Timeline {
             // ok
         } else if (Q4.toString().equals(name)) {
             // ok
-        }else {
+        } else if (Q12.toString().equals(name)){
+            // ok
+        }else if (Q34.toString().equals(name)){
+            // ok
+        }else if(name.matches("M\\d")){
+            // ok
+        }
+        else {
             throw new RuntimeException("fatal: BAD_REQUEST: 无效的时间段名称:" + name);
         }
     }
@@ -107,7 +128,7 @@ public class Timeline {
             // current week
             sql = buildLatestQuery(mysqlDbType, timestampField, calendar.get(Calendar.DAY_OF_WEEK) + " DAY");
         } else if(M.toString().compareTo(name)==0){
-            // current week
+            // current month
             sql = buildLatestQuery(mysqlDbType, timestampField, calendar.get(Calendar.DAY_OF_MONTH) + " DAY");
         } else if(Y.toString().compareTo(name)==0){
             // current year
@@ -124,6 +145,30 @@ public class Timeline {
         } else if (LM3.toString().compareTo(name) == 0) {
             /// latest 3 month
             sql = buildLatestQuery(mysqlDbType, timestampField, "3 MONTH");
+        } else if(M1.toString().compareTo(name)==0){
+            sql = buildMonthQuery(mysqlDbType, timestampField, 1);
+        }else if(M2.toString().compareTo(name)==0){
+            sql = buildMonthQuery(mysqlDbType, timestampField, 2);
+        }else if(M3.toString().compareTo(name)==0){
+            sql = buildMonthQuery(mysqlDbType, timestampField, 3);
+        }else if(M4.toString().compareTo(name)==0){
+            sql = buildMonthQuery(mysqlDbType, timestampField, 4);
+        }else if(M5.toString().compareTo(name)==0){
+            sql = buildMonthQuery(mysqlDbType, timestampField, 5);
+        }else if(M6.toString().compareTo(name)==0){
+            sql = buildMonthQuery(mysqlDbType, timestampField, 6);
+        }else if(M7.toString().compareTo(name)==0){
+            sql = buildMonthQuery(mysqlDbType, timestampField, 7);
+        }else if(M8.toString().compareTo(name)==0){
+            sql = buildMonthQuery(mysqlDbType, timestampField, 8);
+        }else if(M9.toString().compareTo(name)==0){
+            sql = buildMonthQuery(mysqlDbType, timestampField, 9);
+        }else if(M10.toString().compareTo(name)==0){
+            sql = buildMonthQuery(mysqlDbType, timestampField, 10);
+        }else if(M11.toString().compareTo(name)==0){
+            sql = buildMonthQuery(mysqlDbType, timestampField, 11);
+        }else if(M12.toString().compareTo(name)==0){
+            sql = buildMonthQuery(mysqlDbType, timestampField, 12);
         } else if (Q1.toString().compareTo(name) == 0) {
             sql = buildQuarterQuery(mysqlDbType, timestampField, 1);
         } else if (Q2.toString().compareTo(name) == 0) {
@@ -131,6 +176,10 @@ public class Timeline {
         } else if (Q3.toString().compareTo(name) == 0) {
             sql = buildQuarterQuery(mysqlDbType, timestampField, 3);
         } else if (Q4.toString().compareTo(name) == 0) {
+            sql = buildQuarterQuery(mysqlDbType, timestampField, 4);
+        }else if (Q12.toString().compareTo(name) == 0) {
+            sql = buildQuarterQuery(mysqlDbType, timestampField, 4);
+        }else if (Q34.toString().compareTo(name) == 0) {
             sql = buildQuarterQuery(mysqlDbType, timestampField, 4);
         } else {
             throw new RuntimeException("fatal: invalid timeline name: " + name);
@@ -168,7 +217,7 @@ public class Timeline {
      *
      * @param dbType
      * @param column
-     * @param which  1-Q1, 2-Q2, 3-Q3, 4-Q4
+     * @param which  1-Q1, 2-Q2, 3-Q3, 4-Q4, 12-上半年  34-下半年
      * @return
      */
     private String buildQuarterQuery(String dbType, String column, int which) { //季度
@@ -197,6 +246,14 @@ public class Timeline {
                 start = String.format("'%s-10-01'", year);
                 end = String.format("'%s-01-01'", year + 1);
                 break;
+            case 12:
+                start = String.format("'%s-01-01'", year);
+                end = String.format("'%s-07-01'", year);
+                break;
+            case 34:
+                start = String.format("'%s-07-01'", year);
+                end = String.format("'%s-01-01'", year + 1);
+                break;
             default:
                 throw new RuntimeException("这是一个内部异常，出现在 Timeline.buildQQuery() 中的Q 参数");
         }
@@ -216,6 +273,77 @@ public class Timeline {
         throw new RuntimeException("fatal: invalid dbType = " + dbType);
     }
 
+    private String buildMonthQuery(String dbType, String column, int which) { //月份
+
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+
+        String start = "", end = "";
+        switch (which) {
+            case 1:
+                start = String.format("'%s-01-01'", year);
+                end = String.format("'%s-02-01'", year);
+                break;
+            case 2:
+                start = String.format("'%s-02-01'", year);
+                end = String.format("'%s-03-01'", year);
+                break;
+            case 3:
+                start = String.format("'%s-03-01'", year);
+                end = String.format("'%s-04-01'", year);
+                break;
+            case 4:
+                start = String.format("'%s-04-01'", year);
+                end = String.format("'%s-05-01'", year + 1);
+                break;
+            case 5:
+                start = String.format("'%s-05-01'", year);
+                end = String.format("'%s-06-01'", year + 1);
+                break;
+            case 6:
+                start = String.format("'%s-06-01'", year);
+                end = String.format("'%s-07-01'", year + 1);
+                break;
+            case 7:
+                start = String.format("'%s-07-01'", year);
+                end = String.format("'%s-08-01'", year + 1);
+                break;
+            case 8:
+                start = String.format("'%s-08-01'", year);
+                end = String.format("'%s-09-01'", year + 1);
+                break;
+            case 9:
+                start = String.format("'%s-09-01'", year);
+                end = String.format("'%s-10-01'", year + 1);
+                break;
+            case 10:
+                start = String.format("'%s-10-01'", year);
+                end = String.format("'%s-11-01'", year + 1);
+                break;
+            case 11:
+                start = String.format("'%s-11-01'", year);
+                end = String.format("'%s-12-01'", year + 1);
+                break;
+            case 12:
+                start = String.format("'%s-12-01'", year);
+                end = String.format("'%s-01-01'", year+1);
+                break;
+            default:
+                throw new RuntimeException("这是一个内部异常，出现在 Timeline.buildQQuery() 中的Q 参数");
+        }
+
+        if ("mysql".equals(dbType)) {
+            return String.format("%s <= date(%s) and %s > date('%s')", column, end, column, start);
+
+        } else if ("sqlserver".equals(dbType)) {
+            return null;
+        } else if ("oracle".equals(dbType)) {
+            return null;
+        } else if ("Access".equals(dbType)) {
+            return null;
+        }
+
+        throw new RuntimeException("fatal: invalid dbType = " + dbType);
+    }
 
     /**
      * build today query sql
